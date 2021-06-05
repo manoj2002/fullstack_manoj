c269e26615c31f0c1ed2cb897b3cbb6d79002c47
manoj2002 <55148291+manoj2002@users.noreply.github.com>
2021-06-05 17:14:20 +0530

    Add files via upload

---
+++ part3/part-3.2/index.js
@@ -0,0 +1,37 @@
+const { request, response } = require('express')
+const express=require('express')
+const app=express()
+app.use(express.json())
+let persons=[
+  { name: 'Arto Hellas',
+  number: '040-123456',
+  id:1,  
+},
+{ name: 'manoj',
+  number: '040-123465986',
+  id:2,  
+},
+{ name: 'manoj rajulapati',
+  number: '057-45756',
+  id:3,  
+},
+{
+ name: 'binkauy',
+number: '540-1548956',
+id:4,  
+}
+]
+  app.get('/' , (request,response) => {
+      response.send("<h1>Welcome to Phone Book Application</h1>")
+  })
+  app.get('/api/persons', (request,response) =>{
+      response.json(persons)
+  })
+  app.get('/api/info',(request,response) => {
+     const d=new Date();
+     const l=persons.length;
+     response.send(`<p>PhoneBook Has Info For ${l} People</p><p>${d}</p>`)
+  })
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
