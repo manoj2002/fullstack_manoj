66fe87b7df298a478c694511d33a5cc80b4daf7b
manoj2002 <55148291+manoj2002@users.noreply.github.com>
2021-06-07 16:25:14 +0530

    front-end and back-end connection

---
+++ part3/part-3.9-3.11/backend/index.js
@@ -0,0 +1,108 @@
+const { request, response } = require('express')
+const express=require('express')
+const app=express()
+app.use(express.json())
+var morgan = require('morgan')
+const cors = require('cors')
+app.use(cors())
+app.use(express.static('build'))
+morgan.token('data', (req) => {
+  return req.method === 'POST'
+    ? JSON.stringify(req.body)
+    : null
+})
+
+
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.data(req, res)
+  ].join(' ')
+}))
+let persons=[
+  { name: 'Arto Hellas',
+  number: '040-123456',
+  id:1,  
+},
+{ name: 'manoj',
+  number: '040-123465986',
+  id:2,  
+},
+{ name: 'manoj rajulapati',
+  number: '057-45756',
+  id:3,  
+},
+{
+ name: 'binkauy',
+number: '540-1548956',
+id:4,  
+}
+]
+  app.get('/' , (request,response) => {
+      response.send("<h1>Welcome to Phone Book Application</h1>")
+  })
+  app.get('/api/persons', (request,response) =>{
+      response.json(persons)
+  })
+  app.get('/api/info',(request,response) => {
+     const d=new Date();
+     const l=persons.length;
+     response.send(`<p>PhoneBook Has Info For ${l} People</p><p>${d}</p>`)
+  })
+  app.get('/api/persons/:id', (request, response) => {
+    const id =Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if(person)
+    {
+        response.json(person)
+    }
+    else{
+        response.status(404).end()
+    }
+  })
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+     console.log(persons)
+    response.status(204).end()
+  })
+  const newId = () => {
+    const max = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return max + 1
+  }
+  const checkPerson = (name) =>{
+        const m=persons.find(person => person.name.toLowerCase() === name.toLowerCase())
+        return m;
+  }
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body);
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'name or number is missing' 
+      })
+    }
+    else if(checkPerson(body.name))
+    {
+      return response.status(400).json({ 
+        error: 'name must be unique'
+      })
+    }
+     const person = {
+      name: body.name,
+      number: body.number,
+      id: newId(),
+    }
+    persons = persons.concat(person)
+    response.json(person)
+  })
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
